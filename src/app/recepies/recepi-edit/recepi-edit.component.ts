6a69c2fe549b7f669c794ef1eb278ff483aa750e
gone7com <djoshi19@in.ibm.com>
2020-01-04 20:15:16 +0530

    added ingredient solution and image preview

--- src/app/recepies/recepi-edit/recepi-edit.component.ts
+++ src/app/recepies/recepi-edit/recepi-edit.component.ts
@@ -4,7 +4,7 @@ import { recepieService } from '../recepieService.service';
 import { Recepie } from '../recepie.model';
 import { Ingredients } from 'src/app/shared/Ingredients.model';
 import { Route } from '@angular/compiler/src/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recepi-edit',
@@ -19,24 +19,32 @@ export class RecepiEditComponent implements OnInit {
   recepie:Recepie;
   ingredients:string="";
   ingredientsRaw:string[];
-  ingredientMake:String;
+  ingredientMake:any;
   ingredientsName:string;
   ingredientQunatity:number;
   ingredient:Ingredients;
   ingredientArray:Ingredients[]=[];
   recepieEditForm:FormGroup;
-
+  ingredientArrayForm = new FormArray([]);
   constructor(private route:ActivatedRoute,private recepieService:recepieService,private router:Router) { 
     this.recepieEditForm = new FormGroup({
       'name':new FormControl(null),
       'detail': new FormControl(null),
       'image':new FormControl(null),
-      'ingredient':new FormControl(null)
+      'ingredient':new FormControl(null),
+      'ingredientArray': this.ingredientArrayForm
     });
+    console.log(this.ingredientArrayForm);
     if(route.snapshot.params.id){
       this.id=route.snapshot.params.id;
       this.recepie=this.recepieService.getCurrentRecepie(this.id);
-     
+      for(let ing of this.recepie.ingredients){
+        this.ingredientArrayForm.push(new FormGroup({
+          'name' : new FormControl(ing.ingName),
+          'amount': new FormControl(ing.quantity)
+        }))
+      }
+      
         var index:number=0;
       for(this.ingredient of this.recepieService.getCurrentRecepie(route.snapshot.params.id).ingredients){
         this.ingredients+=this.ingredient.ingName+":";
@@ -50,9 +58,14 @@ export class RecepiEditComponent implements OnInit {
         'name': this.recepie.name,
         'detail': this.recepie.description,
         'image': this.recepie.imgPath,
-        'ingredient': this.ingredients
+        'ingredient': this.ingredients,
+        'ingredientArray':this.ingredientArrayForm.value
+
       })
       }
+    
+   
+    
   }
 
   ngOnInit() {
@@ -62,28 +75,26 @@ export class RecepiEditComponent implements OnInit {
 
   addRecepie(){
       if(this.route.snapshot.params.id){
-      this.ingredientsRaw=this.recepieEditForm.value.ingredient.split(",");
-      for(this.ingredientMake of this.ingredientsRaw){
-        var ingredientsArray=this.ingredientMake.split(":");
-        this.ingredientsName=ingredientsArray[0];
-        this.ingredientQunatity = +ingredientsArray[1];
-        this.ingredientArray.push(new Ingredients(this.ingredientsName,this.ingredientQunatity))
+      for(this.ingredientMake of this.recepieEditForm.value.ingredientArray){
+        this.ingredientArray.push(new Ingredients(this.ingredientMake.name,this.ingredientMake.amount))
     }
     console.log(new Recepie(this.recepieEditForm.value.name,this.recepieEditForm.value.detail,this.recepieEditForm.value.image,this.ingredientArray));
     this.recepieService.editRecepie(this.route.snapshot.params.id,new Recepie(this.recepieEditForm.value.name,this.recepieEditForm.value.detail,this.recepieEditForm.value.image,this.ingredientArray));
   
   }
       else{
-        this.ingredientsRaw=this.recepieEditForm.value.ingredient.split(",");
-      for(this.ingredientMake of this.ingredientsRaw){
-        var ingredientsArray=this.ingredientMake.split(":");
-        this.ingredientsName=ingredientsArray[0];
-        this.ingredientQunatity = +ingredientsArray[1];
-        this.ingredientArray.push(new Ingredients(this.ingredientsName,this.ingredientQunatity))
+        for(this.ingredientMake of this.recepieEditForm.value.ingredientArray){
+          this.ingredientArray.push(new Ingredients(this.ingredientMake.name,this.ingredientMake.amount))
       }
       this.recepieService.addRecepie(new Recepie(this.recepieEditForm.value.name,this.recepieEditForm.value.detail,this.recepieEditForm.value.image,this.ingredientArray));
     }
     this.router.navigate(["/recepies/"+this.route.snapshot.params.id]);
   }
-
+addIngredientRow(){
+ var formControl =  new FormGroup({
+  'name' : new FormControl(null),
+  'amount': new FormControl(null)
+});
+this.ingredientArrayForm.push(formControl);
+}
 }
